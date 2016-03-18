ccd623f0d39960c478d7b3ec0570962bffa5e4e0
Márton Miháltz <mmihaltz@gmail.com>
2016-03-18 20:21:50 +0100

    added playground/quntokenapiTest

---
+++ attic/playground/quntokenapiTest/main.cpp
@@ -0,0 +1,22 @@
+#include "qtoken_api.h"
+
+int main(int argc, char** argv)
+{
+    // input file
+    if (argc < 2)
+    {
+        std::cerr << "Missing file name!" << std::endl;
+        exit(0);
+    }
+
+    // create input stringstream from file:
+    std::ifstream in(argv[1]);
+    std::stringstream fst_input;
+    fst_input << in.rdbuf();
+
+    qtoken_print({PREP, SNT, SNTCORR, SNTCORR, TOKEN}, &fst_input, XML);
+
+    std::cout << std::endl;
+
+    return 0;
+}
