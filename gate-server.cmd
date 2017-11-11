54170e27f2da174b16e78cc9fd597d29737f8df1
Peter K <temprimus@gmail.com>
2017-11-11 14:48:32 +0100

    set memory limits

--- gate-server.cmd
+++ gate-server.cmd
@@ -2,4 +2,4 @@
 
 IF "%GATE_HOME%" == "" SET GATE_HOME=.
 
-java -Xmx1g -Xms256m -cp "%GATE_HOME%/bin/gate.jar;%GATE_HOME%/lib/*;gate-server.jar" gate.server.Main
+java -Xmx8g -Xms512m -cp "%GATE_HOME%/bin/gate.jar;%GATE_HOME%/lib/*;gate-server.jar" gate.server.Main
--- gate-server.sh
+++ gate-server.sh
@@ -4,4 +4,4 @@ if [ "$GATE_HOME" == "" ]; then
   GATE_HOME=.
 fi
 
-java -cp "$GATE_HOME/bin/gate.jar:$GATE_HOME/lib/*:gate-server.jar" gate.server.Main $1
+java -Xmx8g -Xms512m -cp "$GATE_HOME/bin/gate.jar:$GATE_HOME/lib/*:gate-server.jar" gate.server.Main $1
