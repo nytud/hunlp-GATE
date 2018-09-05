24f6f21bb7f4227f5d7d08b2e884fbb1885c377a
ivan.mittelholcz <ivan.mittelholcz@gmail.com>
2018-09-05 15:15:26 +0200

    refactoring

--- utils/emconv.py
+++ utils/emconv.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Convert GATE-XML from  e-magyar to TSV.
+"""Convert GATE-XML coming from e-magyar to TSV.
 
-With no FILE, read the stdin and write to stdout. With FIEL(s) write output
+If no FILE, read the stdin and write to stdout. If FILE(s), write output
 to the <filename>.conv.tsv file.
 """
 
@@ -13,17 +13,35 @@ import sys
 
 
 def check_dir(directory):
-    """Check directory exists.
+    """Check if directory exists.
     """
     if not os.path.exists(directory):
         raise argparse.ArgumentError()
     return directory
 
 
+def check_format(form):
+    """Check if format valid.
+    """
+    if form not in ['x', 'c']:
+        raise argparse.ArgumentError()
+    return form
+
+
 def get_args():
     """Get commandline arguments.
     """
     pars = argparse.ArgumentParser(description=__doc__)
+    pars.add_argument(
+            '-f',
+            '--format',
+            help='Format of sentense separations: '
+                 'x (<s> xml tag, default); '
+                 'c (conll, new line) ',
+            required=False,
+            default='x',
+            type=check_format
+    )
     pars.add_argument(
             '-d',
             '--dir',
@@ -40,74 +58,65 @@ def get_args():
     return vars(pars.parse_args())
 
 
-def convert(xml):
-    """Get text (in xml format), return with the converted tsv. Entry point.
+def get_features(annot):
+    """Get an xml element tree, return a dict containing the features for a
+    token.
     """
-    tree = ET.fromstring(xml)
-    root = tree.find('AnnotationSet')
-    res = ['ID', 'string', 'anas', 'feature', 'lemma', 'hfstana', 'pos',
-           'chunk', 'depType', 'depTarget', 'cons', 'NER']
-    res = '\t'.join(res) + '\n'
-    annots = root.findall('Annotation')
-    if len(annots) > 0:
-        sentences = []
-        snt = ''
-        for annot in annots:
-            fullline = ''
-            type = annot.get('Type')
-            tid = annot.get('Id')
-            if type == 'Token':
-                fullline += str(tid) + '\t'
-                fts = {}
-                for feature in annot.findall('Feature'):
-                    pair = {feature.find('Name').text:
-                            feature.find('Value').text}
-                    fts.update(pair)
-                fullline += getLine(fts)
-                snt += fullline
-            elif type == 'Sentence':
-                sentences.append(snt)
-                snt = ''
-        res += '<s>\n' + '</s>\n<s>\n'.join(sentences) + '</s>'
+    res = {}
+    res['ID'] = annot.get('Id')
+    for feature in annot.findall('Feature'):
+        key = feature.find('Name').text
+        value = feature.find('Value').text
+        if value == None:
+            value = ''
+        res[key] = value
     return res
 
 
-def getLine(features):
-    line = ""
-    if 'string' in features and features['string'] is not None:
-        line += features['string']
-    line += "\t"
-    if 'anas' in features and features['anas'] is not None:
-        line += features['anas']
-    line += "\t"
-    if 'feature' in features and features['feature'] is not None:
-        line += features['feature']
-    line += "\t"
-    if 'lemma' in features and features['lemma'] is not None:
-        line += features['lemma']
-    line += "\t"
-    if 'hfstana' in features and features['hfstana'] is not None:
-        line += features['hfstana']
-    line += "\t"
-    if 'pos' in features and features['pos'] is not None:
-        line += features['pos']
-    line += "\t"
-    if 'NP-BIO' in features and features['NP-BIO'] is not None:
-        line += features['NP-BIO']
-    line += "\t"
-    if 'depType' in features and features['depType'] is not None:
-        line += features['depType']
-    line += "\t"
-    if 'depTarget' in features and features['depTarget'] is not None:
-        line += features['depTarget']
-    line += "\t"
-    if 'cons' in features and features['cons'] is not None:
-        line += features['cons']
-    line += "\t"
-    if 'NER-BIO1' in features and features['NER-BIO1'] is not None:
-        line += features['NER-BIO1']
-    line += "\n"
-    return line
+def get_line(features):
+    """Get a dict of token features, return a line of TSV.
+    """
+    columns = ['ID', 'string', 'anas', 'feature', 'lemma', 'hfstana', 'pos',
+               'NP-BIO', 'depType', 'depTarget', 'cons', 'NER-BIO1', ]
+    res = [features.get(key, '') for key in columns]
+    res = '\t'.join(res)
+    return res
+
+
+def convert(xml):
+    """Convert GATE-XML to list of sentences (format independent
+    representation). Entry point.
+    """
+    root = ET.fromstring(xml)
+    sentences = []  # list of sentences
+    snt = []  # list of TSV-lines
+    for annot in root.findall('./AnnotationSet/Annotation'):
+        type_ = annot.get('Type')
+        if type_ == 'Token':
+            features = get_features(annot)
+            line = get_line(features)
+            snt.append(line)
+        elif type_ == 'Sentence':
+            sentences.append(snt)
+            snt = []
+    sentences = ['\n'.join(snt) for snt in sentences]
+    return sentences
+
+
+def format_tsv(sentences, form):
+    """Get list of sentences and format them.
+    """
+    res = ''
+    header = ['ID', 'string', 'anas', 'feature', 'lemma', 'hfstana', 'pos',
+           'chunk', 'depType', 'depTarget', 'cons', 'NER']
+    res = '\t'.join(header) + '\n'
+    if form == 'x':
+        res += '<s>\n'
+        res += '\n</s>\n<s>\n'.join(sentences)
+        res += '\n</s>'
+    elif form == 'c':
+        res += '\n\n'.join(sentences)
+    return res
 
 
 def main():
@@ -116,22 +125,25 @@ def main():
     args = get_args()
 
     def get_path(file_name):
-        """Get input filename and args, return the path of the output file.
+        """Get input filename, return the path of the output file.
         """
         file_name = os.path.abspath(file_name)
         dir_ = args['dir'] if args['dir'] else os.path.dirname(file_name)
         name = os.path.splitext(os.path.basename(file_name))[0]
         name += '.conv.tsv'
         return os.path.join(dir_, name)
+
     if args['FILE']:
+        # read from files
         for file_ in args['FILE']:
-            tsv = convert(file_.read())
+            tsv = format_tsv(convert(file_.read()), args['format'])
             path = get_path(file_.name)
             with open(path, 'w') as file_:
                 print(tsv, file=file_)
     else:
-            tsv = convert(sys.stdin.read())
-            print(tsv)
+        # read from stdin
+        tsv = format_tsv(convert(sys.stdin.read()), args['format'])
+        print(tsv)
     return
 
 
